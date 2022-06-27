b486ece20df0580d8fd5034f84621941097b0bea
Luis Emilio Grijalba <luisegrijalba8@gmail.com>
2022-06-27 09:11:23 -0500

    final prog III

--- src/controllers/cotroller.user.js
+++ src/controllers/cotroller.user.js
@@ -3,6 +3,28 @@ const bcrypt = require("bcrypt-nodejs");
 const User = require("../models/user.model");
 const jwt = require("../services/jwt");
 
+/* const sgMail = require('@sendgrid/mail')
+
+
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+const msg = {
+  to: 'test@example.com', // Change to your recipient
+  from: 'test@example.com', // Change to your verified sender
+  subject: 'Sending with SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+}
+sgMail
+  .send(msg)
+  .then(() => {
+    console.log('Email sent')
+  })
+  .catch((error) => {
+    console.error(error)
+  }) */
+
+
 function signUp(req, res) {
   //console.log('inicial --->',User(req.body));
   const user = new User();
